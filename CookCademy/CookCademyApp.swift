303ac252a699a386926d2da86d34f1de856c5c8a
Sarah Watremet <sarah.watremet@gmail.com>
2022-05-31 15:19:07 +0200

    Initial Commit

---
+++ CookCademy/CookCademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookCademyApp.swift
+//  CookCademy
+//
+//  Created by Sarah Watremet on 31/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct CookCademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
