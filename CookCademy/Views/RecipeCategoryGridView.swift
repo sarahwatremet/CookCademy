5eccee99e94bef6b5a25f8a4210d50ab130bd88d
Sarah Watremet <sarah.watremet@gmail.com>
2022-06-01 16:20:20 +0200

    added categories with grids

---
+++ CookCademy/Views/RecipeCategoryGridView.swift
@@ -0,0 +1,53 @@
+//
+//  RecipeCategoryGridView.swift
+//  CookCademy
+//
+//  Created by Sarah Watremet on 01/06/2022.
+//
+
+import SwiftUI
+
+struct RecipeCategoryGridView: View {
+    @StateObject private var recipeData = RecipeData()
+    
+    var body: some View {
+        let columns = [GridItem(), GridItem()]
+            NavigationView {
+              ScrollView {
+                LazyVGrid(columns: columns, content: {
+                  ForEach(MainInformation.Category.allCases,
+                          id: \.self) { category in
+                      NavigationLink(
+                        destination: RecipesListView(category: category)
+                            .environmentObject(recipeData),
+                        label: {
+                            CategoryView(category: category)
+                        })
+                  }
+                })
+              }
+              .navigationTitle("Categories")
+            }
+        }
+    }
+    
+struct CategoryView: View {
+    let category: MainInformation.Category
+
+    var body: some View {
+        ZStack {
+            Image(category.rawValue)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .opacity(0.35)
+            Text(category.rawValue)
+                .font(.title)
+        }
+    }
+}
+    
+struct RecipeCategoryGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeCategoryGridView()
+    }
+}
